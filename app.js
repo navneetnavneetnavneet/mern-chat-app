cf6fba9027de0db5a87803308abb9d19821dc791
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-11-09 22:27:41 +0530

    database connection setup

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ require("dotenv").config({
 const express = require("express");
 const app = express();
 
+// db connection
+require("./config/db").connectDatabase();
+
 // create server
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
