13900d4043d3a9ddedb1942b3fd4e0dd368caf0b
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-11-21 22:03:49 +0530

    move socket on seperate file socket.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config({
   path: "./.env",
 });
 const express = require("express");
-const app = express();
+const { server, app } = require("./socket/socket");
 const logger = require("morgan");
 const ErrorHandler = require("./utils/ErrorHandler");
 const { generateErrors } = require("./middlewares/errors");
@@ -49,66 +49,6 @@ app.all("*", (req, res, next) => {
 app.use(generateErrors);
 
 // create server
-const server = app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
-});
-
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:5173",
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("A user is connected", socket.id);
-
-  // Setup user
-  socket.on("setup", (userData) => {
-    if (!userData || !userData._id) {
-      console.error("Invalid user data for setup");
-      return;
-    }
-    socket.join(userData._id); // Join user's personal room
-    socket.emit("connected");
-    console.log(`User ${userData._id} joined their personal room`);
-  });
-
-  // Join a specific room
-  socket.on("join-room", (room) => {
-    if (!room) {
-      console.error("Invalid room");
-      return;
-    }
-    socket.join(room); // Join chat room
-    console.log(`User joined room: ${room}`);
-  });
-
-  // Handle new message
-  socket.on("new-message", (newMessage) => {
-    if (!newMessage || !newMessage.chatId || !newMessage.senderId) {
-      console.error("Invalid message data received");
-      return;
-    }
-
-    const { chatId, senderId } = newMessage;
-    const chat = newMessage.chatId;
-
-    if (!chat.users || !Array.isArray(chat.users)) {
-      return console.error("Chat users not defined or invalid format");
-    }
-
-    // console.log("New message received:", newMessage);
-
-    // Broadcast message to other users in the chat
-    chat.users.forEach((user) => {
-      if (user._id.toString() === senderId._id.toString()) return; // Exclude sender
-      socket.emit("msg", newMessage);
-
-      socket.to(user?._id).emit("message-received", newMessage);
-    });
-  });
-
-  socket.on("disconnecting", () => {
-    console.log(`User disconnected: ${socket.id}`);
-  });
 });
